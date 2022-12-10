a0f6d38355afbaa5320c588a958f0d3348e711f4
Kerry Doan <kerry.l.doan@gmail.com>
2022-12-10 13:46:55 -0700

    Switch to mocha and chai.

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,5 +1,7 @@
+import { expect } from 'chai';
+
 describe('Sample tests', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
+    it('should pass', () => {
+        expect(true).to.equal(true);
+    });
 });
